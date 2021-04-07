5c9c6b64283baa82ae5182e8aac8b81aacb23388
K1ssSh0t <72368646+K1ssSh0t@users.noreply.github.com>
2021-04-06 22:45:20 -0500

    Update RandomPassword.py

--- RandomPassword.py
+++ RandomPassword.py
@@ -1,33 +1,23 @@
 import random
 
-
-
-leterrs = ["a","A,","b","B","c","C","d","D","e","E","f","F","g","G","h","H","i","I","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u","U","v","V","w","W","x","X","y","Y","z","Z"]
-numbers =["0","1","2","3","4","5","6","7","8","9"]
-characters =["!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[",",","~"]
-
-i =0
-password = "";
-
-while i <4:
-    l = int(random.randint(0,51))
-    a = str(leterrs[l])
-    n = int(random.randint(0,9))
-    b = str(numbers[n])
-    ca  = int(random.randint(0,22))
-    c = str(characters[ca])
-
-    password = password+a + b + c
+array = ["a","A,","b","B","c","C","d","D","e","E","f","F","g","G","h","H","i","I","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u","U","v","V","w","W","x","X","y","Y","z","Z","0","1","2","3","4","5","6","7","8","9","!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[",",","~"]
 
 
 
+i=0
+
+password="";
+
+while i<12:
+
+    a=int(random.randint(0,82))
+    b=str(array[a])
+
+    password = password +b
 
     i+=1
 
-
-
 palabra = ''.join(random.sample(password,len(password)))
-
 
 print("Random Password of 12 Characters")
 
