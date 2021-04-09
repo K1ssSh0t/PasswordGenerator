fc1e63a3aca2b3501006e0e4f94644453b50a7f7
K1ssSh0t <72368646+K1ssSh0t@users.noreply.github.com>
2021-04-08 21:58:06 -0500

    Update RandomPassword.py

--- RandomPassword.py
+++ RandomPassword.py
@@ -2,13 +2,13 @@ import random
 
 array = ["a","A,","b","B","c","C","d","D","e","E","f","F","g","G","h","H","i","I","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u","U","v","V","w","W","x","X","y","Y","z","Z","0","1","2","3","4","5","6","7","8","9","!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[",",","~"]
 
-
+length=int(input("Password Length: "))
 
 i=0
 
 password="";
 
-while i<12:
+while i<length:
 
     a=int(random.randint(0,82))
     b=str(array[a])
@@ -19,6 +19,6 @@ while i<12:
 
 palabra = ''.join(random.sample(password,len(password)))
 
-print("Random Password of 12 Characters")
+print(f"Random Password of {length} Characters")
 
 print(palabra)
